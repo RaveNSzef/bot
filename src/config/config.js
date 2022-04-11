08285af08b9d7c5402d9203070c59d36a97640d0
RaveNSzef <103451766+RaveNSzef@users.noreply.github.com>
2022-04-11 20:11:43 +0200

    Initial commit

---
+++ src/config/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    token: "OTYyODE0MDkyMTMxODU2NTI0.YlNAfw.pN6FlilO4pT9PiB0eZwAjWGu_YM",                                                           // TOKEN BOTA
+    prefix: "!",                                                                                                                    // PREFIX
+    owner: "559282792966389762",                                                                                                    // ID
+    avatar: "https://cdn.discordapp.com/icons/720260551535624222/a_b9f239a470c2ee660a771814840cdf60.webp",                                // AVATAR PRZY AUTORZE EMBED (W POSTACI URL)
+    name: "🪐WRLRP",                                                                                                               // NAZWA PRZY AUTORZE EMBED
+    ip: "51.38.134.76:30120",
+    haslo: "-",                                                                                                       // IP SERWERA FIVEM (RAZEM Z PORTEM)
+    webhooklogi1: "559282792966389762",                                                                                             // DANE 1 DO WEBHOOKA NA LOGI
+    webhooklogi2: "SigSwHK9DIJft1ohoKnAXtHwXfnOr-YurAs0rmK8BnyygLUg3F1Ch3GEBO0B9JcXyCtN",                                           // DANE 2 DO WEBHOOKA NA LOGI
+    everyone: "559282792966389762",
+    darkweb: "559282792966389762",
+    kanallogi: "559282792966389762",
+    kategoriaticket: "559282792966389762",
+    kategoriaarchiwum: "559282792966389762",
+    rolaobsługi: "559282792966389762",
+    logiig1: "559282792966389762",
+    logiig2: "SigSwHK9DIJft1ohoKnAXtHwXfnOr-YurAs0rmK8BnyygLUg3F1Ch3GEBO0B9JcXyCtN",
+    kanalig1: "559282792966389762",
+    kanalig2: "E5dodh_20Vn_pzWv38-ZrE0crVLd7Lp8dQtlVMY9J5d7uLzc1JY1g1PCaSCATxMhF1tR",
+    kanalinstagram: "559282792966389762",
+    warny: "559282792966389762",
+    rolamute: "559282792966389762",
+    kanalliczbabanow: "559282792966389762",
+    kanalliczbaosoball: "559282792966389762",
+    kanalliczbaonline: "559282792966389762",
+    guildid: "559282792966389762",
+    kanalpowitania: "559282792966389762",
+    kanaladminpodania: "559282792966389762",
+    kanalwynikipodania: "559282792966389762",
+    kanalrekordonline: "559282792966389762",
+    kanalnowy: "559282792966389762",
+    kanalbanroom: "559282792966389762",
+    kanalkontakt: "559282792966389762",
+    kanalkontaktadmin: "559282792966389762"
+}
